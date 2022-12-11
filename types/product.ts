a8bbe53d1b6df5b3d346fb5ff657eb6157e29700
pierre Edimo <pedimonkoe@yahoo.com>
2022-12-11 22:54:38 +0100

    the category and product types

---
+++ types/product.ts
@@ -0,0 +1,16 @@
+export type Product = {
+    _id: string,
+    name: string,
+    description: string,
+    weight: number,
+    imageUrl: string,
+    sellingPrice: number,
+    locationId: string,
+    bookMarked: boolean,
+    quantity: number,
+    arrivalDate?: Date,
+    buyingPrice: number,
+    originLabel: string,
+    productSku: string,
+    locationName: string,
+}
