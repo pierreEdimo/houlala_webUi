22a44f1771851a89ddfa91d5c3e3eaa339006ccd
pierre Edimo <pedimonkoe@yahoo.com>
2022-12-11 23:07:02 +0100

    add export

--- components/home.location.list.tsx
+++ components/home.location.list.tsx
@@ -5,16 +5,18 @@ import styles from "../styles/location.module.scss";
 export function HomeLocationList() {
     const locationList = useLocationList("https://api.houlala.store/discovery-orchestrator/locations/store");
 
-    return(
+    return (
         <div>
             <h2>Magasins</h2>
-            <div className={styles.horizontalList} >
+            <div className={styles.horizontalList}>
                 {
                     locationList.map((location) => (
-                        <HorizontalLocationContainer key={location.id} location={location} />
+                        <HorizontalLocationContainer key={location.id} location={location}/>
                     ))
                 }
             </div>
         </div>
     );
 }
+
+export {}
