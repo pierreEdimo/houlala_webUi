28e721ef70ad4e38f78575ad716f6e0ce4dd1f7f
pierre Edimo <pedimonkoe@yahoo.com>
2023-01-01 08:56:23 +0100

    creation du product.container

--- components/product.container.tsx
+++ components/product.container.tsx
@@ -1,22 +1,33 @@
 import {Product} from "../types/product";
 import styles from "../styles/product.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 
 export function ProductContainer({product}: { product: Product }) {
     return (
 
-           <div className={styles.productContainer}>
-               <div className={styles.imageContainer}>
-                   <Image className={styles.productImage}
-                          src={product.imageUrl}
-                          alt="product-image"
-                          layout="fill"
-                          objectFit="contain"
-                          priority={true}
-                   />
-               </div>
-               <h3>{product.name}</h3>
-               <p>{product.sellingPrice} FCFA</p>
-           </div>
+        <Link
+            style={{
+                cursor: "pointer"
+            }}
+            href={{
+                pathname: '/product/[pid]',
+                query: {pid: product._id}
+            }}>
+            <div className={styles.productContainer}>
+                <div className={styles.imageContainer}>
+                    <Image className={styles.productImage}
+                           src={product.imageUrl}
+                           alt="product-image"
+                           layout="fill"
+                           objectFit="contain"
+                           priority={true}
+                    />
+                </div>
+                <h3>{product.name}</h3>
+                <p>{product.sellingPrice} FCFA</p>
+            </div>
+        </Link
+        >
     )
 }
