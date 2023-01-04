74ddd1a3d9705ce839c0462ac2adb34d32460da2
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-04 07:43:49 +0100

    changer le hooks pour swr

--- pages/product/[pid].tsx
+++ pages/product/[pid].tsx
@@ -1,8 +1,6 @@
 import {NextPage} from "next";
 import styles from "../../styles/product.module.scss";
 import {useRouter} from "next/router";
-import {Product} from "../../types/product";
-import {useProduct} from "../../hooks/product.hooks";
 import {AppBar} from "../../components/app.bar";
 import {NestedLayout} from "../../components/nested.layout";
 import Image from "next/image";
@@ -10,26 +8,30 @@ import ReactMarkdown from "react-markdown";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faChevronLeft, faBagShopping} from "@fortawesome/free-solid-svg-icons";
 import {faHeart} from "@fortawesome/free-regular-svg-icons";
+import {useProduct} from "../../swrHooks/product.hooks";
 
 const Product: NextPage = () => {
     const router = useRouter();
     const {pid} = router.query;
     const PRODUCT_Url = process.env.NEXT_PUBLIC_PRODUCT_URL;
-    const product: Product = useProduct(`${PRODUCT_Url}/${pid}`);
+    const {product, isLoading, isError} = useProduct(`${PRODUCT_Url}/${pid}`);
+
+    if (isLoading) return <div>...Loading</div>
+
+    if (isError) return <div>...Error</div>
 
     return (
         <>
-            <AppBar title={"Houla la"}/>
             <NestedLayout>
                 <div className={styles.productPageContainer}>
                     <span style={{
                         fontWeight: "bolder",
                         fontSize: "32px"
-                    }}>{product.name}</span>
+                    }}>{product!.name}</span>
                     <div className={styles.productPage}>
                         <div className={styles.productImageContainer}>
                             <div className={styles.productImage}>
-                                <Image src={product.imageUrl}
+                                <Image src={product!.imageUrl}
                                        style={{borderRadius: "20px"}}
                                        alt={"product-image"}
                                        layout={"fill"}
@@ -38,14 +40,14 @@ const Product: NextPage = () => {
                             </div>
                         </div>
                         <div className={styles.infoProductContainer}>
-                            <p className={styles.sellingPrice}>{product.sellingPrice} XAF</p>
+                            <p className={styles.sellingPrice}>{product!.sellingPrice} XAF</p>
                             <div className={styles.productCount}>
                                 <span>-</span>
                                 <p>1</p>
                                 <span>+</span>
                             </div>
                             <div>
-                                <p style={{margin: "0"}}>Vendeur: <b>{product.locationName}</b></p>
+                                <p style={{margin: "0"}}>Vendeur: <b>{product!.locationName}</b></p>
                             </div>
                             <div className={styles.buttonContainer}>
                                 <button>
@@ -62,7 +64,7 @@ const Product: NextPage = () => {
                         </div>
                     </div>
                     <ReactMarkdown>
-                        {product.description}
+                        {product!.description}
                     </ReactMarkdown>
                 </div>
                 <div className={styles.mobileProductPageContainer}>
@@ -84,11 +86,11 @@ const Product: NextPage = () => {
                         <span style={{
                             fontWeight: "bold",
                             fontSize: "22px"
-                        }}>{product.name}</span>
+                        }}>{product!.name}</span>
                     </div>
                     <div className={styles.productImageContainer}>
                         <div className={styles.productImage}>
-                            <Image src={product.imageUrl}
+                            <Image src={product!.imageUrl}
                                    layout={"fill"} objectFit={"contain"}
                                    priority={true}
                                    alt={"product-image"}/>
@@ -96,7 +98,7 @@ const Product: NextPage = () => {
                     </div>
                     <div className={styles.mobileInfoProductContainer}>
                         <div className={styles.mobileProductCount}>
-                            <p className={styles.sellingPrice}>{product.sellingPrice} XAF</p>
+                            <p className={styles.sellingPrice}>{product!.sellingPrice} XAF</p>
                             <div className={styles.productCount}>
                                 <span>-</span>
                                 <p>1</p>
@@ -104,7 +106,7 @@ const Product: NextPage = () => {
                             </div>
                         </div>
                         <div>
-                            <p style={{margin: "0"}}>Vendeur: <b>{product.locationName}</b></p>
+                            <p style={{margin: "0"}}>Vendeur: <b>{product!.locationName}</b></p>
                         </div>
                         <div className={styles.buttonContainer}>
                             <button>
@@ -120,7 +122,7 @@ const Product: NextPage = () => {
                         </div>
                     </div>
                     <ReactMarkdown>
-                        {product.description}
+                        {product!.description}
                     </ReactMarkdown>
                 </div>
             </NestedLayout>
