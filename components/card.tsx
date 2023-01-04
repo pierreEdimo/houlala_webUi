d29e7b249e5c64e35b24c466f2fc6cd1f6feebc2
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-04 07:46:00 +0100

    supprimer le width du parametre

--- components/card.tsx
+++ components/card.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "../styles/card.module.scss"
 
-export function Card({children, width}: { children: React.ReactNode, width?: number }) {
+export function Card({children}: { children: React.ReactNode }) {
     return (
-        <div className={styles.cardContainer} style={{width: width}}>
+        <div className={styles.cardContainer}>
             {children}
         </div>
     )
