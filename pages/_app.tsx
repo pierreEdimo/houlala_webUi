be2cd23bef9968145a9718424438d5f9d675eb0f
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-04 07:44:14 +0100

    ajouter le AppBar dans le _apps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ import bell from "../public/images/notification.png";
 import user from "../public/images/user.png";
 import Head from "next/head";
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import {AppBar} from "../components/app.bar";
 
 function MyApp({Component, pageProps}: AppProps) {
 
@@ -27,27 +28,28 @@ function MyApp({Component, pageProps}: AppProps) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                 <title>{title}</title>
             </Head>
+            <AppBar title={"Houla la"}/>
             <Component {...pageProps} />
             <footer className="bottom-bar">
-               <NestedLayout>
-                   <div className="bottom-bar-content">
-                       <Link href="/">
-                           <Image src={house} width={25} height={25} alt="house-image" />
-                       </Link>
-                       <Link href="/discover">
-                           <Image src={category} width={25} height={25} alt="category-image"/>
-                       </Link>
-                       <Link href="/cart">
-                           <Image src={cart} width={25} height={25} alt="corbeille-image"/>
-                       </Link>
-                       <Link href="/notification">
-                           <Image src={bell} width={25} height={25} alt="notification-image"/>
-                       </Link>
-                       <Link href="/user">
-                           <Image src={user} width={25} height={25} alt="user-image"/>
-                       </Link>
-                   </div>
-               </NestedLayout>
+                <NestedLayout>
+                    <div className="bottom-bar-content">
+                        <Link href="/">
+                            <Image src={house} width={25} height={25} alt="house-image"/>
+                        </Link>
+                        <Link href="/discover">
+                            <Image src={category} width={25} height={25} alt="category-image"/>
+                        </Link>
+                        <Link href="/cart">
+                            <Image src={cart} width={25} height={25} alt="corbeille-image"/>
+                        </Link>
+                        <Link href="/notification">
+                            <Image src={bell} width={25} height={25} alt="notification-image"/>
+                        </Link>
+                        <Link href="/user">
+                            <Image src={user} width={25} height={25} alt="user-image"/>
+                        </Link>
+                    </div>
+                </NestedLayout>
             </footer>
         </RecoilRoot>
     );
