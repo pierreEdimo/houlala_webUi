e51ea1d9c524a394ce927be933d71327104b677d
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-04 07:50:39 +0100

    ajouter le swr hooks du produit

--- components/home.product.list.tsx
+++ components/home.product.list.tsx
@@ -1,22 +1,13 @@
-import {useProductList} from "../hooks/product.hooks";
-import styles from "../styles/product.module.scss";
-import {ProductContainer} from "./product.container";
+import ProductGrid from "./product.grid";
 
 
 export function HomeProductList() {
     const PRODUCT_URL = process.env.NEXT_PUBLIC_PRODUCT_URL;
-    const productList = useProductList(`${PRODUCT_URL}`);
 
     return (
         <div>
             <h2>Fruits & Legumes</h2>
-            <div className={styles.homeProductListContainer}>
-                {
-                    productList.map((product) => (
-                        <ProductContainer key={product._id} product={product}/>
-                    ))
-                }
-            </div>
+            <ProductGrid url={`${PRODUCT_URL}`}/>
         </div>
     );
 }
