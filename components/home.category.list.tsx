8e081690c30fd093b433a7351da9b23f07977cb9
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-04 07:50:22 +0100

    ajouter le hooks dans home.category.list

--- components/home.category.list.tsx
+++ components/home.category.list.tsx
@@ -1,16 +1,19 @@
-import {useCategoryList} from "../hooks/category.hooks";
 import styles from "../styles/category.module.scss";
+import {useCategoryList} from "../swrHooks/category.hooks";
 import {CategoryContainer} from "./category.container";
 
 export function HomeCategoryList() {
     const CATEGORY_URL = process.env.NEXT_PUBLIC_CATEGORY_URL;
-    const categoryList = useCategoryList(`${CATEGORY_URL}`);
+    const {categories, isLoading, isError} = useCategoryList(`${CATEGORY_URL}`);
+
+    if (isLoading) return <div>...Loading</div>
+    if (isError) return <div>...Error</div>
 
     return (
         <div>
             <h2>Categories</h2>
             <div className={styles.homeCategoryContainer}>
-                {categoryList.map((category) => (
+                {categories?.map((category) => (
                     <CategoryContainer key={category._id} category={category}/>
                 ))}
             </div>
