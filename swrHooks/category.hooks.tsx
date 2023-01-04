57be0623689b5acc8b8034f0bad00ef4cee9e7f8
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-04 07:47:35 +0100

    creer le swr category.hooks

---
+++ swrHooks/category.hooks.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import useSWR from "swr";
+import {Category} from "../types/category";
+
+function useCategoryList(categoryUrl: string) {
+    const fetcher = async (url: string) => await axios.get<Category[]>(url).then((res) => res.data);
+    const {data, error, isLoading} = useSWR<Category[]>(categoryUrl, fetcher);
+    return {
+        categories: data,
+        isLoading,
+        isError: error
+    };
+}
+
+function useCategory(categoryUrl: string) {
+    const fetcher = async (url: string) => await axios.get<Category>(url).then((res) => res.data);
+    const {data, error, isLoading} = useSWR<Category>(categoryUrl, fetcher);
+    return {
+        category: data,
+        isLoading,
+        isError: error
+    };
+}
+
+export {useCategoryList, useCategory}
