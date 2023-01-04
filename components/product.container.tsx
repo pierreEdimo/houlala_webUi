ff3d0129f09cb612ead0aeccdff7e6a3f4f5e651
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-04 07:52:31 +0100

    creer le product.container

--- components/product.container.tsx
+++ components/product.container.tsx
@@ -6,28 +6,23 @@ import Link from "next/link";
 export function ProductContainer({product}: { product: Product }) {
     return (
 
-        <Link
-            style={{
-                cursor: "pointer"
-            }}
-            href={{
-                pathname: '/product/[pid]',
-                query: {pid: product._id}
-            }}>
-            <div className={styles.productContainer}>
-                <div className={styles.imageContainer}>
-                    <Image className={styles.productImage}
-                           src={product.imageUrl}
-                           alt="product-image"
-                           layout="fill"
-                           objectFit="contain"
-                           priority={true}
-                    />
+        <>
+            <Link
+                style={{
+                    cursor: "pointer"
+                }}
+                href={{
+                    pathname: '/product/[pid]',
+                    query: {pid: product._id}
+                }}>
+                <div>
+                    <div className={styles.productContainer}>
+                        <Image src={product.imageUrl} alt={"product-image"} layout={"fill"} objectFit={"contain"}/>
+                    </div>
+                    <h3>{product.name}</h3>
+                    <p>{product.sellingPrice} FCFA</p>
                 </div>
-                <h3>{product.name}</h3>
-                <p>{product.sellingPrice} FCFA</p>
-            </div>
-        </Link
-        >
+            </Link>
+        </>
     )
 }
