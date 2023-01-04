03b4f7d202934f2bb4a0c685237ede08677869d9
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-04 07:45:20 +0100

    Ameliorer l'avatar pour prendre le thumbnail en compte

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,24 +1,39 @@
 import Image from "next/image";
 import avatarStyles from "../styles/avatar.module.scss";
+import React from "react";
 
-
-export function Avatar({imageUrl, width, height,}: {
-    imageUrl: string,
-    width: number,
-    height: number
-}) {
-    return (
-        <div style={{
-            width: width,
-            height: height,
-            position: "relative"
-        }}>
-            <Image className={avatarStyles.avatar}
-                   src={imageUrl} alt="category-image"
-                   layout="fill"
-                   objectFit="cover"
-                   priority={true}
-            />
-        </div>
-    );
+type AvatarProps = {
+    imageUrl: string;
+    type: string;
 }
+
+const Avatar: React.FC<AvatarProps> = ({imageUrl, type}: { imageUrl: string, type: string }) => {
+    switch (type) {
+        case "thumbnail":
+            return (
+                <div className={avatarStyles.avatarContainer}>
+                    <Image className={avatarStyles.thumbnail}
+                           src={imageUrl} alt="category-image"
+                           layout="fill"
+                           objectFit="cover"
+                           priority={true}
+                    />
+                </div>
+            )
+        case "avatar":
+        default:
+            return (
+                <div className={avatarStyles.avatarContainer}>
+                    <Image className={avatarStyles.avatar}
+                           src={imageUrl} alt="category-image"
+                           layout="fill"
+                           objectFit="cover"
+                           priority={true}
+                    />
+                </div>
+            )
+    }
+
+}
+
+export default Avatar;
