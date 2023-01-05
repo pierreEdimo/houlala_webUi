6a0339531e5900518c9c036714e90aedc15aa018
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-05 07:45:15 +0100

    creer le backbutton

---
+++ components/back.button.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronLeft} from "@fortawesome/free-solid-svg-icons";
+import {useRouter} from "next/router";
+
+type BackButtonProps = {
+    title: string
+}
+
+const BackButton: React.FC<BackButtonProps> = ({title}: { title: string }) => {
+    const router = useRouter();
+    return (
+        <>
+            <div
+                onClick={() => router.back()}
+                style={{
+                    display: "flex",
+                    gap: "10px",
+                    alignItems: "center",
+                    cursor: "pointer"
+                }}>
+                <FontAwesomeIcon icon={faChevronLeft} style={{fontSize: "20px"}}/>
+                <h2>{title}</h2>
+            </div
+            >
+        </>
+    )
+}
+
+export default BackButton;
