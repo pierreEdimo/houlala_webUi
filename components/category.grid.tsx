ec002ccb0fff545af02665825aa55659681c69b3
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-05 07:46:05 +0100

    ajouter l'error handling dans category.grid

--- components/category.grid.tsx
+++ components/category.grid.tsx
@@ -1,14 +1,17 @@
-import {useCategoryList} from "../hooks/category.hooks";
 import styles from "../styles/category.module.scss";
+import {useCategoryList} from "../swrHooks/category.hooks";
 import GridCategoryContainer from "./grid.category.container";
 
 export function CategoryGrid() {
     const CATEGORY_URL = process.env.NEXT_PUBLIC_CATEGORY_URL;
-    const categoryList = useCategoryList(`${CATEGORY_URL}`);
+    const {categories, isLoading, isError} = useCategoryList(`${CATEGORY_URL}`);
+
+    if (isError) return <div>...Error</div>
+    if (isLoading) return <div>...Loading </div>
 
     return (
         <div className={styles.gridContainer}>
-            {categoryList.map((category) => (
+            {categories?.map((category) => (
                 <GridCategoryContainer key={category._id} category={category}/>
             ))}
         </div>
