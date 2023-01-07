4e9352556938e9d71a21358a7f5df7594c8070bc
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-07 11:05:03 +0100

    ajouter le Backbutton

--- pages/discover/index.tsx
+++ pages/discover/index.tsx
@@ -1,12 +1,19 @@
 import {NextPage} from "next"
 import {NestedLayout} from "../../components/nested.layout";
-import { CategoryGrid } from "../../components/category.grid";
+import {CategoryGrid} from "../../components/category.grid";
+import styles from "../../styles/category.module.scss";
+import BackButton from "../../components/back.button";
 
 const Index: NextPage = () => {
     return (
         <div>
             <NestedLayout>
-                <h2>Decouvrez nos produits par Categories</h2>
+                <div>
+                    <h2 className={styles.categoryTitle}>Decouvrez nos produits par Categorie</h2>
+                    <div className={styles.backButtonContainer}>
+                        <BackButton title={"Categorie"}/>
+                    </div>
+                </div>
                 <CategoryGrid/>
             </NestedLayout>
         </div>
