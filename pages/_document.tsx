4d7e954b9fa8b1974b47aabb39b8a1adeb905a2e
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-08 12:18:08 +0100

    ajouter le font-weight bolder

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Html, Head, Main, NextScript} from 'next/document'
 
-export default function     Document() {
+export default function Document() {
     return (
         <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com"/>
                 <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheetheet"/>
             </Head>
             <body>
-            <Main />
-            <NextScript />
+            <Main/>
+            <NextScript/>
             </body>
         </Html>
     )
