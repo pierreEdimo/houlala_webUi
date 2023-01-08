a3599191cf04e4aeede0820bf78750ac3c071283
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-08 15:03:07 +0100

    ajouter le Link

--- components/vertical.location.container.tsx
+++ components/vertical.location.container.tsx
@@ -3,6 +3,7 @@ import {Card} from "./card";
 import styles from "../styles/location.module.scss";
 import React from "react";
 import Avatar from "./avatar";
+import Link from "next/link";
 
 type VerticalLocationProps = {
     location: Location
@@ -12,19 +13,21 @@ const VerticalLocationContainer: React.FC<VerticalLocationProps> = ({location}) 
     return (
         <>
             <Card>
-                <div className={styles.verticalLocation}>
-                    <div>
-                        <div className={styles.imageContainer}>
-                            <Avatar imageUrl={location.imageUrl} type={"avatar"}/>
+                <Link href={`/location/${location.id}`}>
+                    <div className={styles.verticalLocation}>
+                        <div>
+                            <div className={styles.imageContainer}>
+                                <Avatar imageUrl={location.imageUrl} type={"avatar"}/>
+                            </div>
+                        </div>
+                        <div>
+                            <h3 className={styles.title}>{location.name}</h3>
+                            <h4 className={styles.smallTitle}>{location.category.name}</h4>
+                            <h5 className={styles.smallTitle}>{location.address.city}</h5>
+                            <p className={styles.descriptionText}>{location.description}</p>
                         </div>
                     </div>
-                    <div>
-                        <h3 className={styles.title}>{location.name}</h3>
-                        <h4 className={styles.smallTitle}>{location.category.name}</h4>
-                        <h5 className={styles.smallTitle}>{location.address.city}</h5>
-                        <p className={styles.descriptionText}>{location.description}</p>
-                    </div>
-                </div>
+                </Link>
             </Card>
         </>
     )
