ecc4c2da8ee5c51d1f0f812b02aab097fadae828
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-08 15:01:00 +0100

    ajouter le Spinner

--- components/home.category.list.tsx
+++ components/home.category.list.tsx
@@ -1,12 +1,18 @@
 import styles from "../styles/category.module.scss";
 import {useCategoryList} from "../swrHooks/category.hooks";
 import CategoryContainer from "./category.container";
+import Spinner from "./spinner";
 
 export function HomeCategoryList() {
     const CATEGORY_URL = process.env.NEXT_PUBLIC_CATEGORY_URL;
     const {categories, isLoading, isError} = useCategoryList(`${CATEGORY_URL}`);
 
-    if (isLoading) return <div>...Loading</div>
+    if (isLoading) return (
+        <>
+            <Spinner/>
+        </>
+    )
+
     if (isError) return <div>...Error</div>
 
     return (
