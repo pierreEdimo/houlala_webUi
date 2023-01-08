6a439afe67ba457800b9c4763b5d0e569b2462d0
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-08 14:59:27 +0100

    ajouter le Spinner

--- pages/location/[lid].tsx
+++ pages/location/[lid].tsx
@@ -2,16 +2,31 @@ import {NextPage} from "next";
 import {NestedLayout} from "../../components/nested.layout";
 import BackButton from "../../components/back.button";
 import {useRouter} from "next/router";
+import {useLocation} from "../../swrHooks/location.hooks";
+import Spinner from "../../components/spinner";
 
 const Location: NextPage = () => {
     const router = useRouter();
     const {lid} = router.query;
+    const LOCATION_URL = process.env.NEXT_PUBLIC_LOCATION_URL;
+    const {location, isLoading, isError} = useLocation(`${LOCATION_URL}/${lid}`);
+
+    if (isLoading) return (
+        <>
+            <Spinner/>
+        </>
+    )
+
+    if (isError) return (
+        <div>
+            ....Error
+        </div>
+    )
 
     return (
         <>
             <NestedLayout>
-                <BackButton title={"Location"}/>
-                <p>{lid}</p>
+                <BackButton title={`${location?.name}`}/>
             </NestedLayout>
         </>
     )
