dbc9e9a2592959d1cc88466bb8116365f09dac43
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-08 10:23:58 +0100

    refactoring du component sub.category dans fruits.lettuces

--- components/fruits.lettuces.tsx
+++ components/fruits.lettuces.tsx
@@ -4,6 +4,7 @@ import {useSubCategoryList} from "../swrHooks/sub.category.hooks";
 import SubCategoryContainer from "./sub.category.container";
 import ProductGrid from "./product.grid";
 import BackButton from "./back.button";
+import {useRouter} from "next/router";
 
 type FruitsLettucesProps = {
     categoryId: string,
@@ -14,7 +15,8 @@ const FruitsLettuces: React.FC<FruitsLettucesProps> = ({categoryId, name}: { cat
 
     const CATEGORY_URL = process.env.NEXT_PUBLIC_SUBCATEGORY_URL;
     const PRODUCT_URL = process.env.NEXT_PUBLIC_PRODUCT_URL;
-    const {categories, isLoading, isError} = useSubCategoryList(`${CATEGORY_URL}/category/${categoryId}`)
+    const {categories, isLoading, isError} = useSubCategoryList(`${CATEGORY_URL}/category/${categoryId}`);
+    const router = useRouter();
 
 
     if (isLoading) return <div>...Loading</div>
@@ -27,7 +29,11 @@ const FruitsLettuces: React.FC<FruitsLettucesProps> = ({categoryId, name}: { cat
                 <BackButton title={name}/>
                 <div className={styles.subCategoryList}>
                     {categories?.map((category) => (
-                        <SubCategoryContainer key={category._id} subCategory={category}/>
+                        <SubCategoryContainer
+                            onClick={() => router.push(`/sub-category/${category._id}`)}
+                            key={category._id}
+                            label={category.label}
+                            thumbNailUrl={category.thumbNailUrl}/>
                     ))}
                 </div>
                 <div style={{height: "20px"}}></div>
