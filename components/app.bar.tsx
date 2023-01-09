557b22880ee5395c2eaf8a6b12c8d4cadc9d78c8
pierre Edimo <pierre.nkoe@vema-eg.de>
2023-01-09 09:07:23 +0100

    creer le nav search dans le app.bar

--- components/app.bar.tsx
+++ components/app.bar.tsx
@@ -3,42 +3,20 @@ import Image from "next/image";
 import houlala from "../public/images/houlala1.png";
 import Link from "next/link";
 import search from "../public/images/search.png";
-import React, {useCallback, useEffect,} from "react";
+import React from "react";
 import styles from "../styles/app.bar.module.scss";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faSearch} from "@fortawesome/free-solid-svg-icons";
 import ButtonContainer from "./button.container";
 import category from "../public/images/category.png";
 import cart from "../public/images/cart.png";
 import {useRouter} from "next/router";
-import {useRecoilState} from "recoil";
-import {PreviewState} from "../atoms/preview.state";
 import NotificationDropdown from "./notification.dropdown";
 import OptionsDropdown from "./options.dropdown";
+import NavSearch from "./nav.search";
 
 
 const AppBar = ({title}: { title: string }) => {
-    const router = useRouter();
-    const [isOpen, setIsOpen] = useRecoilState(PreviewState);
 
-    const showPreview = useCallback((event: React.MouseEvent<HTMLInputElement>) => {
-        event.stopPropagation();
-        if (!isOpen) {
-            document.getElementById("searchComponent")!.style.display = "block";
-            setIsOpen(true);
-        }
-    }, [isOpen, setIsOpen]);
-
-
-    useEffect(() => {
-        window.onclick = () => {
-            if (isOpen) {
-                document.getElementById("searchComponent")!.style.display = "none";
-                setIsOpen(false);
-            }
-        };
-    })
-
+    const router = useRouter()
 
     return (
         <header className={styles.appHeader}>
@@ -69,20 +47,7 @@ const AppBar = ({title}: { title: string }) => {
                             </div>
                         </Link>
                     </div>
-                    <div className={styles.searchContainer}>
-                        <form>
-                            <input type={"text"} onClick={showPreview} placeholder={"Recherche"}
-                                   className={styles.textSearch}/>
-                            <button className={styles.searchButton}>
-                                <FontAwesomeIcon icon={faSearch}/>
-                            </button>
-                        </form>
-                        <div className={styles.searchPreview} id={"searchComponent"}>
-                            <p>
-                                Preview
-                            </p>
-                        </div>
-                    </div>
+                    <NavSearch/>
                     <div style={{display: "flex", gap: "20px"}}>
                         <ButtonContainer onClick={() => router.push('/discover')} imageSrc={category}/>
                         <ButtonContainer onClick={() => router.push('/cart')} imageSrc={cart}/>
